34fedfd229fd77e5ad038f48184f5f264bab9fdb
Julian0liveira <143848613+Julian0liveira@users.noreply.github.com>
2023-09-20 16:51:35 -0300

    Exercicio de sala de aula 20/09

---
+++ exercicio.ts
@@ -0,0 +1,81 @@
+class Celular{
+    nome:string = '';
+    marca:string = '';
+    modelo:string = '';
+    memoria:string = '';
+    valor:number = 0;
+
+    setNome(nome:string){
+        this.nome = nome;
+    }
+    getNome(){
+        return this.nome 
+    }
+
+    setMarca(marca:string){
+        this.marca = marca;
+    }
+    getMarca(){
+        return this.marca;
+    }
+    
+    setModelo(modelo:string){
+        this.modelo = modelo;
+    }
+    getModelo(){
+        return this.modelo;
+    }
+
+    setMemoria(memoria:string){
+        this.memoria = memoria;
+    }
+    getMemoria(){
+        return this.memoria;
+    }
+
+    setValor(valor:string){
+        this.valor = valor;
+    }
+    getValor(){
+        return this.valor;
+    }
+}
+
+class Vendas{
+    produto:Celular[] = [];
+    constructor(produtos:Celular[]){
+        for(let i=0; i< produtos.length; i++){
+            this.produto.push(produtos[i]);
+        }
+    }
+
+    somarValor(){
+        for(let value of this.produtos){
+            let valorTotal = 0;
+            valorTotal = this.produtos[value];
+            valorTotal = valorTotal + valorTotal;
+            console.log("Valor total de vendas: "+valorTotal);
+        }
+    }
+}
+
+let celular1 = new Celular();
+celular1.setNome = 'EDGE';
+celular1.setMarca = 'Motorola';
+celular1.setModelo = '200';
+celular1.setMemoria = '8GB';
+celular1.setValor = 1300;
+
+let celular2 = new Celular();
+celular2.setNome = 'Galaxy';
+celular2.setMarca = 'Samsumg';
+celular2.setModelo = 'S21 Fe';
+celular2.setMemoria = '8GB';
+celular2.setValor = 1200;
+
+let totalCelular:Celular[] = [];
+totalCelular.push(celular1);
+totalCelular.push(celular2);
+
+let venda = new Vendas();
+venda.somarValor(totalCelular);
