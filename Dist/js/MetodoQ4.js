e74bc5a842db40ef5b33d0dfa5dccc4a6e1f13a7
Julian0liveira <julian0liveira@hotmail.com>
2023-09-21 17:20:59 -0300

    Questão 4

---
+++ Dist/js/MetodoQ4.js
@@ -0,0 +1,5 @@
+"use strict";
+function metodo(parametro1, parametro2) {
+    return parametro1 + parametro2;
+}
+console.log("Resultado da soma: " + metodo(50, 50));
---
+++ src/MetodoQ4.ts
@@ -0,0 +1,5 @@
+function metodo(parametro1:number,parametro2:number){
+    return parametro1 + parametro2;
+}
+
+console.log("Resultado da soma: "+metodo(50,50));
