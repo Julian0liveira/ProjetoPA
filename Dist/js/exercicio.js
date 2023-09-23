104d9f681960f14a9562e66bcc8dc662f5667294
Julian0liveira <julian0liveira@hotmail.com>
2023-09-22 22:31:30 -0300

    Novo Array memoria

--- Dist/js/exercicio.js
+++ Dist/js/exercicio.js
@@ -40,17 +40,25 @@ class Celular {
 }
 class Vendas {
     constructor(produtos) {
+        this.memoria = [];
         this.produto = [];
         for (let i = 0; i < produtos.length; i++) {
             this.produto.push(produtos[i]);
         }
     }
     somarValor() {
+        let valorTotal = 0;
         for (let i = 0; i < this.produto.length; i++) {
-            let valorTotal; //*************** 
-            valorTotal = this.produto[i].getValor;
-            valorTotal += valorTotal; //valorTotal;
-            console.log("Valor total de vendas: " + valorTotal);
+            valorTotal += this.produto[i].getValor();
+            //valorTotal += valorTotal; //valorTotal;
+        }
+        this.memoria.push(valorTotal);
+        console.log("Valor total de vendas: " + valorTotal);
+    }
+    exibirMemoria() {
+        console.log("Imprimindo os valores da memoria: ");
+        for (let i = 0; i < this.memoria.length; i++) {
+            console.log(this.memoria[i] + ",");
         }
     }
 }
@@ -65,10 +73,29 @@ celular2.setNome('Galaxy');
 celular2.setMarca('Samsumg');
 celular2.setModelo('S21 Fe');
 celular2.setMemoria('8GB');
-celular2.setValor(1200);
+celular2.setValor(100);
 let totalCelular = [];
 totalCelular.push(celular1);
 totalCelular.push(celular2);
-console.log(celular1);
+//console.log(celular1);
 let venda = new Vendas(totalCelular);
 venda.somarValor();
+venda.exibirMemoria();
+let celular3 = new Celular();
+celular3.setNome('Poco');
+celular3.setMarca('Xiaomi');
+celular3.setModelo('F5');
+celular3.setMemoria('8GB');
+celular3.setValor(1000);
+let celular4 = new Celular();
+celular4.setNome('Galaxy');
+celular4.setMarca('Samsumg');
+celular4.setModelo('S23+');
+celular4.setMemoria('8GB');
+celular4.setValor(200);
+let totalCelular2 = [];
+totalCelular2.push(celular3);
+totalCelular2.push(celular4);
+let venda2 = new Vendas(totalCelular2);
+venda2.somarValor();
+venda2.exibirMemoria();
--- src/exercicio.ts
+++ src/exercicio.ts
@@ -42,6 +42,7 @@ class Celular{
 }
 
 class Vendas{
+    memoria:number[] = [];
     produto:Celular[] = [];
     constructor(produtos:Celular[]){
         for(let i=0; i< produtos.length; i++){
@@ -50,11 +51,20 @@ class Vendas{
     }
 
     somarValor(){
-        for(let i=0; i< this.produto.length; i++){
-            let valorTotal:any; //*************** 
-            valorTotal = this.produto[i].getValor;
-            valorTotal += valorTotal; //valorTotal;
-            console.log("Valor total de vendas: "+valorTotal);
+        let valorTotal:number = 0;
+        for(let i=0; i< this.produto.length; i++){ 
+            valorTotal += this.produto[i].getValor();
+            //valorTotal += valorTotal; //valorTotal;
+        }
+        this.memoria.push(valorTotal);
+        console.log("Valor total de vendas: "+valorTotal);
+
+    }
+
+    exibirMemoria(){
+        console.log("Imprimindo os valores da memoria: ");
+        for(let i=0; i < this.memoria.length; i++){
+            console.log(this.memoria[i]+",");
         }
     }
 }
@@ -71,15 +81,38 @@ celular2.setNome('Galaxy');
 celular2.setMarca('Samsumg');
 celular2.setModelo('S21 Fe');
 celular2.setMemoria('8GB');
-celular2.setValor(1200);
+celular2.setValor(100);
 
 let totalCelular:Celular[] = [];
 totalCelular.push(celular1);
 totalCelular.push(celular2);
 
-console.log(celular1);
+//console.log(celular1);
 
 
 
 let venda = new Vendas(totalCelular);
 venda.somarValor();
+venda.exibirMemoria();
+
+let celular3 = new Celular();
+celular3.setNome('Poco');
+celular3.setMarca('Xiaomi');
+celular3.setModelo('F5');
+celular3.setMemoria('8GB');
+celular3.setValor(1000);
+
+let celular4 = new Celular();
+celular4.setNome('Galaxy');
+celular4.setMarca('Samsumg');
+celular4.setModelo('S23+');
+celular4.setMemoria('8GB');
+celular4.setValor(200);
+
+let totalCelular2:Celular[] = [];
+totalCelular2.push(celular3);
+totalCelular2.push(celular4);
+
+let venda2 = new Vendas(totalCelular2);
+venda2.somarValor();
+venda2.exibirMemoria();
