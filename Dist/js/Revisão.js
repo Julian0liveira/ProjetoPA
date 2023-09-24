e6e8cfc79897f64b4b6748f4d7a6f30c8e03dcab
Julian0liveira <julian0liveira@hotmail.com>
2023-09-24 20:24:08 -0300

    .

---
+++ Dist/js/Revisão.js
@@ -0,0 +1,9 @@
+"use strict";
+let soma = () => {
+    return 2 + 2;
+};
+console.log(soma());
+function Soma() {
+    return 3 + 2;
+}
+console.log(Soma() + soma());
---
+++ src/Revisão.ts
@@ -0,0 +1,11 @@
+let soma = () => { //FUNÇÃO ANÔNIMA
+    return 2+2;
+}
+
+console.log(soma());
+
+function Soma(){
+    return 3+2;
+}
+
+console.log(Soma() + soma());
